58d2f8cdde8a7f3ceb9df9f1494662e8993d5026
coollove0406noa <coollove0406@gmail.com>
2025-09-22 03:01:03 +0900

    Update CardImage.tsx

--- components/CardImage.tsx
+++ components/CardImage.tsx
@@ -1,15 +1,6 @@
 // components/CardImage.tsx
 import Image from "next/image";
-
 export default function CardImage({ src, alt }: { src: string; alt: string }) {
-  return (
-    <div style={{ position: "relative", width: "100%", aspectRatio: "2 / 3" }}>
-      <Image
-        src={src}
-        alt={alt}
-        fill
-        sizes="(max-width: 768px) 60vw, 300px"
-      />
-    </div>
-  );
+  return <Image src={src} alt={alt} width={300} height={450} />;
 }
+
