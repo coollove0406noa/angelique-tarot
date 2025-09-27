ac5c9ba39870a6d772376fe43266a891dc70c57b
coollove0406noa <coollove0406@gmail.com>
2025-09-28 04:11:45 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // app/page.tsx
-import OneCardTarot2 from "@/components/OneCardTarot2";
+import OneCardTarot from "../components/OneCardTarot2";
 export default function Home() {
   return (
     <main className="p-6">
